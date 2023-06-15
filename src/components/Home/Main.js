b0280862c275fb332fab832f1b6e3691225f9fe2
unknown <rafaykiyani1135@gmail.com>
2023-06-15 19:08:40 +0500

    changes done

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -7,8 +7,8 @@ import Faq from '../Menhome/Faqsection'
 import OurDr from '../Menhome/Ourdr'
 import Appointment from '../Menhome/Appointment'
 import Blog from '../Menhome/Blog'
-import { FloatingWhatsApp } from 'react-floating-whatsapp-button'
-import 'react-floating-whatsapp-button/dist/index.css'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+//import 'react-floating-whatsapp-button/dist/index.css'
 import Review from '../reviews/trustpilot'
 
 function Main() {
@@ -24,7 +24,7 @@ function Main() {
       <Appointment/>
       <Blog/>
       <div style={{height:"auto",width:"auto",zIndex:"1000",position:"absolute",padding:"50px"}}>
-      <FloatingWhatsApp autoOpenTimeout="0" size="50px" phone="+355 69 375 5065" message='' popupMessage="Hello! How can we help you?" position='left'/>
+      <FloatingWhatsApp accountName="Andent"phoneNumber="+355 69 375 5065" darkMode="true" allowClickAway="true" allowEsc="true"/>
       </div>
       <Review/>
     </>
--- src/components/reviews/trustpilot.js
+++ src/components/reviews/trustpilot.js
@@ -4,11 +4,13 @@ function Review() {
   return (
     <div className='container' style={{paddingBottom:"50px"}}>
     <div className='row justify-content-center'>
-    <div className='col-lg-12'>
-    <iframe src='https://cdn.trustindex.io/amp-widget.html#14a092a12fb16057ce863991d95' title="google reviews" sandbox='allow-scripts allow-same-origin' layout='fixed-height' height='257' resizable='resizable'><span overflow></span></iframe>
+    <div className='col-lg-12 text-center'>
+    <h1 >Google Reviews</h1>
+    <iframe src='https://cdn.trustindex.io/amp-widget.html#14a092a12fb16057ce863991d95' title="google reviews" sandbox='allow-scripts allow-same-origin'  resizable='resizable' height="506"></iframe>
     </div>
-    <div className='col-lg-12'>
-    <iframe src='https://cdn.trustindex.io/amp-widget.html#492565312cf460514b764dc37a2' title="trustpilot reviews" sandbox='allow-scripts allow-same-origin' layout='fixed-height' height='257' resizable='resizable'><span overflow></span></iframe>
+    <div className='col-lg-12 text-center'>
+    <h1 >Trustpilot Reviews</h1>
+    <iframe src='https://cdn.trustindex.io/amp-widget.html#492565312cf460514b764dc37a2' title="trustpilot reviews" sandbox='allow-scripts allow-same-origin' resizable='resizable' height="506"></iframe>
     </div>
     </div>
     </div>
